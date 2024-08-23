516aa52af78f1a40762ab3a2b281d3ca54a05ec9
J.D.SICO <101049344+SICO-171617@users.noreply.github.com>
2024-08-23 08:24:06 +0800

    Add files via upload

---
+++ ComputerAverageApp/ComputerAverageApp/ComputerAverageProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerAverageApp
+{
+    internal class ComputerAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            // [] signifies array
+            float[] user_grades = new float[5];
+            float sum_of_grades = 0; // This code means that the float will initialize from 0
+
+            //Input loop so that Users can enter multiple values before the console execute the next instruction
+            Console.WriteLine($"Enter your grades:");
+            for (int i = 0; i < 5; i++)
+            {
+                user_grades[i] = Convert.ToSingle(Console.ReadLine());
+                sum_of_grades += user_grades[i];
+
+            }
+
+            float grade_average = sum_of_grades / 5;
+            float rounded_grade_average = (int)Math.Round(grade_average); //Instead of (float), I turned it to (int) instead to round the numbers to in values
+
+            Console.WriteLine($"The average of the grades is: {grade_average} \n The rounded average grade is: " + {rounded_grade_average});
+           
+
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ DataTypesApp/DataTypesApp/DataTypesProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesApp
+{
+    internal class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the pieces of apple: ");
+            int apple_count = Convert.ToInt32(Console.ReadLine()); //Convert.ToInt32() performs conversions and returns int representation of input  
+
+            Console.WriteLine("Enter total price of " + apple_count + " apple : ");
+            float apple_price = Convert.ToSingle(Console.ReadLine()); //Convert.ToSingle32() performs conversions and returns  float  representation of input
+
+            Console.WriteLine("The total price of " + apple_count + " is " + apple_price); //Concatenations
+
+            Console.WriteLine("The value of the converted price is " + (int)apple_price); //Data type conversion
+
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
