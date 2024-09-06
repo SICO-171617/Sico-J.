50d94db85c98890e9204a3a0479ba4d1a639873a
J.D.SICO <101049344+SICO-171617@users.noreply.github.com>
2024-09-06 15:18:30 +0800

    Add files via upload

---
+++ Lab4Act1/Lab4Act1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Act1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Puting value in a data type
+            const double Price = 32.50;
+
+            Console.WriteLine("Enter the number of apples you want to buy: ");
+            int No_of_apples = Convert.ToInt32(Console.ReadLine());
+
+            double total_price_of_apple = No_of_apples * Price;
+
+            int totalPriceWholeNumber = (int)Math.Round(total_price_of_apple);//Converts double into integer
+
+            Console.WriteLine($"The total price is: {total_price_of_apple:F2}");//String interpolation with 2 decimal points for the double value 
+
+            Console.WriteLine($"The rounded total price is: {totalPriceWholeNumber}");
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
